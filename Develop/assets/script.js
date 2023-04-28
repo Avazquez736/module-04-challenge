04ae05348ac3c9f3c1e2b31faf7f817f3ce45cd8
Alvaro Vazquez <alvarovazquez@MacBook-Air.local>
2023-04-28 00:30:14 -0600

    added game-over.html

--- Develop/assets/script.js
+++ Develop/assets/script.js
@@ -1,1 +1,15 @@
-let question = $('.question')
+
+var question = document.getElementsByClassName('question');
+let time = document.getElementsByClassName('time')
+let startButton= document.getElementById('start-button')
+let highScore = document.getElementById('high-score')
+let remainingSeconds=45
+let correctAnswers = []
+
+
+  let score= document.getElementById('score')
+
+
+  if(correctAnswers){
+    
+  }
--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # module-04-challenge
+This project is a multiple choice quiz that is timed and uses javaScript and the DOM. Displays a score and saves a high score from previous attempts
